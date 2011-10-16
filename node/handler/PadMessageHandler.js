72c85a6095c3e3d42b2d4b6588e89383d14224ee
Techwraith <derickson@yammer-inc.com>
2011-10-15 19:44:14 -0700

    0 not 10

--- node/handler/PadMessageHandler.js
+++ node/handler/PadMessageHandler.js
@@ -693,7 +693,7 @@ function handleClientReady(client, message)
         //access was granted
         if(statusObject.accessStatus == "grant")
         {
-          if(pad2sessions[message.padId] && pad2sessions[message.padId].length > 0) {
+          if(pad2sessions[message.padId] && pad2sessions[message.padId].length > 10) {
             client.json.send({accessStatus: "padFull"});
             return; // don't send any more messages
           }
