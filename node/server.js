5d5f9ed6c68b0ae14f8e85bda06867b8820a25f9
marogers <marogers@yammer-inc.com>
2011-10-17 12:51:54 -0700

    fix socket.io transport list

--- node/server.js
+++ node/server.js
@@ -458,7 +458,7 @@ async.waterfall([
 
     //init socket.io and redirect all requests to the MessageHandler
     var io = socketio.listen(app);
-    io.set('transports', ['xhr-polling', 'htmlfile', 'flashsocket', 'jsonp-polling']);
+    io.set('transports', ['xhr-polling', 'jsonp-polling']);
 
     var socketIOLogger = log4js.getLogger("socket.io");
     io.set('logger', {
