08a3c9419193ff3e5695c720dcd408387bb9d4e4
Matt Knopp <mknopp@yammer-inc.com>
2011-10-21 18:39:52 -0700

    12proc/server for prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :branch,     'master' unless exists?(:branch)
-set :ports,      [9001, 9002, 9003, 9004, 9005, 9006]
+set :ports,      (9001..9012).map
 
 server 'paddie-001.sjc1.yammer.com', :paddie
 server 'paddie-002.sjc1.yammer.com', :paddie
